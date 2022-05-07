89edab0da0446936bc3c70b7d20af865b3488a18
Gustavo <103332795+JeguinhoXD@users.noreply.github.com>
2022-05-07 13:23:17 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -12,16 +12,27 @@ class Spell():
     self.damage = dmg
 
 def CastSpell(attacker, defender):
-  rSpellIndex = random.randint(0, len(attacker.spells)-1) >
-  spell = attacker.spells[rSpellIndex] #Pega a spell da li>
+  rSpellIndex = random.randint(0, len(attacker.spells)-1) #Randomiza um valor de 0 até o tamanho da lista
+  spell = attacker.spells[rSpellIndex] #Pega a spell da lista, baseado no valor randomizado
 
-  print(attacker.name + " usou a habilidade: " + spell.nam>
+  print(attacker.name + " usou a habilidade: " + spell.name)
 
-  defender.health -= spell.damage #Remove o dano da spell >
+  defender.health -= spell.damage #Remove o dano da spell da vida do personagem
 
-  print(defender.name + " foi atingido com " + str(spell.d>
-  print(defender.name + " está agora com " + str(defender.>
+  print(defender.name + " foi atingido com " + str(spell.damage) + " de dano")
+  print(defender.name + " está agora com " + str(defender.health) + " de vida")
 
 
-heroSpells = [Spell("choque fantasmagorico", 100), Spell(">
-enemySpells = [Spell("golpe fantastico", 100), Spell("dest>
+heroSpells = [Spell("choque fantasmagorico", 100), Spell("trindade onipotente", 800), Spell("choque fantasmagorico", 100)]
+enemySpells = [Spell("golpe fantastico", 100), Spell("destruidor de almas",200), Spell("ataque especial",250)]
+
+hero = Personagem("Jethacu", 1000, heroSpells)
+enemy = Personagem("noiado", 30, enemySpells)
+
+
+while(hero.health > 0 and enemy.health > 0):
+  print("")
+  CastSpell(hero, enemy)
+  if(enemy.health <= 0): break
+  print("")
+  CastSpell(enemy, hero)
